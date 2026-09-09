472709e7e033dbdee747dbf478fa9e60217e5d37
Colin Doherty <colinjdoherty@outlook.com>
2026-09-09 15:00:09 -0700

    fix poison overriding all poison sources

--- src/main/java/org/agmas/noellesroles/mixin/infected/ClearInfectedWithPoisonClearsMixin.java
+++ src/main/java/org/agmas/noellesroles/mixin/infected/ClearInfectedWithPoisonClearsMixin.java
@@ -2,12 +2,13 @@ package org.agmas.noellesroles.mixin.infected;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.At;
 import dev.doctor4t.wathe.cca.GameWorldComponent;
 import dev.doctor4t.wathe.cca.PlayerPoisonComponent;
 import net.minecraft.entity.player.PlayerEntity;
 import org.agmas.noellesroles.Noellesroles;
-import org.agmas.noellesroles.config.NoellesRolesConfig;
-import org.agmas.noellesroles.executioner.ExecutionerPlayerComponent;
 import org.agmas.noellesroles.infected.InfectedPlayerComponent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -25,11 +26,13 @@ public class ClearInfectedWithPoisonClearsMixin {
             if (GameWorldComponent.KEY.get(player.getWorld()).isRole(poisoner, Noellesroles.INFECTED)) {
                 InfectedPlayerComponent.KEY.get(player).infector = poisoner;
                 InfectedPlayerComponent.KEY.get(player).infectedTicks = 1;
+                return;
             }
         }
+        original.call(ticks, poisoner);
     }
-    @WrapMethod(method = "reset")
-    private void noBackfire(Operation<Void> original) {
+    @Inject(method = "reset", at = @At("HEAD"))
+    private void noBackfire(CallbackInfo ci) {
         InfectedPlayerComponent.KEY.get(player).infectedTicks = 0;
     }
 }
